f8be107d60dd365dfdb9570294389d549a4c2ae9
Louis <yienlouis470@gmail.com>
2024-11-15 17:21:51 +0300

    complete assignments

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,21 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink to="/" className="nav-link" end>
+        Home
+      </NavLink>
+      <NavLink to="/directors" className="nav-link">
+        Directors
+      </NavLink>
+      <NavLink to="/actors" className="nav-link">
+        Actors
+      </NavLink>
     </nav>
-    );
-};
+  );
+}
 
 export default NavBar;
